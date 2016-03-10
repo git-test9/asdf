06a758086d2c751252645f59ac2f6b8f4487eecb
git-test9 <gittest9@gmail.com>
2016-03-10 19:23:25 +0100

    Dodati geteri i seteri

--- Project/src/studenti/Student.java
+++ Project/src/studenti/Student.java
@@ -1,8 +1,24 @@
 package studenti;
 
 public class Student {
-	
+
 	private String name;
 	private String index;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getIndex() {
+		return index;
+	}
+
+	public void setIndex(String index) {
+		this.index = index;
+	}
 
 }
